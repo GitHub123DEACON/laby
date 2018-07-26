df03e721b74dc0d13107cd06c2effaa9d4730787
EthanS12345 <41691017+EthanS12345@users.noreply.github.com>
2018-07-26 10:46:45 -0400

    Add files via upload

---
+++ Assets/MouseFollow.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseFollow : MonoBehaviour
+{
+    Vector2 lookDirection;
+    Vector2 smoothingVector;
+    public float sensitivity = 5.0f;
+    public float smoothing = 2.0f;
+    GameObject character;
+    // Use this for initialization
+    void Start()
+    {
+        character = this.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mouseDirection = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")); //
+        mouseDirection = Vector2.Scale(mouseDirection, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+        smoothingVector.x = Mathf.Lerp(smoothingVector.x, mouseDirection.x, 1f / smoothing);
+        smoothingVector.y = Mathf.Lerp(smoothingVector.y, mouseDirection.y, 1f / smoothing);
+        lookDirection += smoothingVector;
+        lookDirection.y = Mathf.Clamp(lookDirection.y, -30f, 10f);
+        transform.localRotation = Quaternion.AngleAxis(-lookDirection.y, Vector3.right);
+        character.transform.localRotation = Quaternion.AngleAxis(lookDirection.x, character.transform.up);
+
+    }
+}
---
+++ Assets/playercode.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercode : MonoBehaviour {
+    public Rigidbody rb;
+    int speed = 10;
+
+	// Use this for initialization
+	void Start () {
+        rb = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb.velocity = new Vector3(0, 0, 5);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            rb.velocity = new Vector3(0, 0, -5);
+        }
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            rb.velocity = new Vector3(5, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            rb.velocity = new Vector3(-5, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        { rb.velocity = new Vector3(0, 5, 0); }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        { rb.velocity = new Vector3(0, 0, 0); }
+
+
+        if (Input.GetKeyUp(KeyCode.W))
+        { rb.velocity = new Vector3(0, 0, 0); }
+
+        if (Input.GetKeyUp(KeyCode.S))
+        { rb.velocity = new Vector3(0, 0, 0); }
+
+
+        if (Input.GetKeyUp(KeyCode.D))
+        { rb.velocity = new Vector3(0, 0, 0); }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        { rb.velocity = new Vector3(0, 0, 0); }
+
+
+    }
+
+
+
+}
